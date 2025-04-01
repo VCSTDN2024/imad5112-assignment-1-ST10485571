0ec6f9c626e6a0c27d5c99aa21d1b2b035f89d44
ST10485571 <st10485571@vcconnect.edu.za>
2025-04-01 22:33:27 +0200

    initial code

--- app/src/main/java/vcmsa/ci/mealsuggestionapp/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/mealsuggestionapp/MainActivity.kt
@@ -2,6 +2,7 @@ package vcmsa.ci.mealsuggestionapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,7 +18,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnClear: Button
     private lateinit var btnExit: Button
     private lateinit var resultTextView: TextView
-    private lateinit var textView: TextView
+
+
+    private var timeInt: EditText? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,33 +35,80 @@ class MainActivity : AppCompatActivity() {
         btnClear = findViewById(R.id.button5)
         btnExit = findViewById(R.id.button6)
         resultTextView = findViewById(R.id.textView3)
+        timeInt = findViewById(R.id.timeEditText)
 
-        btnBreakfast. setOnClickListener() {
+        btnBreakfast.setOnClickListener() {
             //Show breakfast options when button is clicked
-            resultTextView.text =
-                "Breakfast: club sandwich, english breakfast, pancakes, omemlette, oats, cereal with a drink, juice or tea"
+            val userInput = timeInt?.text.toString().trim().toInt()
+
+            when(userInput){
+                in 600..1200 -> resultTextView.text = "Breakfast: club sandwich, english breakfast, pancakes, omemlette, oats, cereal with a drink, juice or tea"
+
+                else -> {
+                    resultTextView.text = "Incorrect time input.Outside Breakfast Suggestion!"
+                }
+            }
+         //   resultTextView.text =
+          //      "Breakfast: club sandwich, english breakfast, pancakes, omemlette, oats, cereal with a drink, juice or tea"
         }
-        btnLunch. setOnClickListener() {
+        btnLunch.setOnClickListener() {
             //Show lunch options when button is clicked
-            resultTextView.text =
-                "Lunch: pizza, burger and fires, slap chips, kota,wraps, cheese sandwich with juice, a drink or tea"
+            val userInput = timeInt?.text.toString().trim().toInt()
+
+            when(userInput) {
+                in 1200..1700 -> resultTextView.text = "Lunch: pizza, burger and fires, slap chips, kota,wraps, cheese sandwich with juice, a drink or tea"
+
+                else -> {
+                    resultTextView.text = "Incorrect time input. Outside Lunch Suggestion!"
+                }
+            }
+           // resultTextView.text =
+              //  "Lunch: pizza, burger and fires, slap chips, kota,wraps, cheese sandwich with juice, a drink or tea"
         }
-        btnDinner. setOnClickListener() {
+        btnDinner.setOnClickListener() {
             //Show dinner options when button is clicked
-            resultTextView.text =
-                "Dinner: rice and mince, mac and cheese, chicken curry, stick beef and noodles, pap and pork, vegetables with tea, coffee, or juice"
+            val userInput = timeInt?.text.toString().trim().toInt()
+
+            when(userInput) {
+                in 1700..2300 -> resultTextView.text = "Dinner: rice and mince, mac and cheese, chicken curry, stick beef and noodles, pap and pork, vegetables with tea, coffee, or juice"
+
+                else -> {
+                    resultTextView.text = "Incorrect time input. Outside Dinner Suggestion!"
+                }
+            }
+          //  resultTextView.text =
+             //   "Dinner: rice and mince, mac and cheese, chicken curry, stick beef and noodles, pap and pork, vegetables with tea, coffee, or juice"
         }
-        btnSnack. setOnClickListener() {
+        btnSnack.setOnClickListener() {
             //Show snack options when button is clicked
-            resultTextView.text = "Snacks: sweets, chips, fruits, peanuts, ice-cream"
+            val userInput = timeInt?.text.toString().trim().toInt()
+
+            when(userInput) {
+                in 100..600 -> resultTextView.text = "Snacks: just have a class of water"
+                in 1100..1200 -> resultTextView.text = "Snacks: sweets, chips, fruits, peanuts, ice-cream, fruits"
+                in 900..1000 -> resultTextView.text = "Tea Break."
+
+                else -> {
+                    resultTextView.text = "Incorrect time input. Outside Snack Suggestion!"
+                }
+            }
+            //resultTextView.text = "Snacks: sweets, chips, fruits, peanuts, ice-cream"
         }
-        btnClear. setOnClickListener() {
-            // Clear the text in the TextView when button is clicked
+        btnClear.setOnClickListener() {
+            // Clear the text in the TextView and EditText when button is clicked
+            timeInt?.text?.clear()
             resultTextView.text = ""
         }
-        btnExit. setOnClickListener() {
+        btnExit.setOnClickListener() {
             //Close the current activity or application when button is clicked
             finish()
         }
     }
-}
+
+
+
+
+
+
+    }
+
